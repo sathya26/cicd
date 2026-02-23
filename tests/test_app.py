4d600c28cd31634c9025baef92735e938e4aff42
sathyakrishna <sathyakrishnapalani@gmail.com>
2026-02-23 09:18:14 +0530

    Fix line endings to LF

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,11 @@
 from fastapi.testclient import TestClient
 from app import app
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 client = TestClient(app)
+
 
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "CI/CD Working!"}
-
