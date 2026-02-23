c1d65806484308c882a61f9da3b425916932b055
sathyakrishna <sathyakrishnapalani@gmail.com>
2026-02-23 08:18:22 +0530

    Enable cicd work docker push

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def read_root():
     return {"message": "CI/CD Working!"}
-assert True
 
+@app.get("/health")
+def health():
+    return {"status": "ok"}
